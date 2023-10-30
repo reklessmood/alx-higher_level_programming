66f7cf8acce37e4d3d025cd04485c0ddcc65f697
reklessmood <nizarbellamqaddam@gmail.com>
2023-10-30 23:47:49 +0100

    fixing

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,26 @@
 #include "lists.h"
+
 /**
- * check_cycle - check if singly linked list has a cycle
+ * check_cycle - checks if a linked list contains a cycle
  * @list: linked list to check
- * Return: 1 or 0
-*/
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow;
-	listint_t *fast;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	if (list == NULL || list->next == NULL)
+	if (!list)
 		return (0);
 
-	slow = list;
-	fast = list->next;
-
-	while (fast != NULL && fast->next != NULL)
+	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
-
 		if (slow == fast)
 			return (1);
 	}
+
 	return (0);
 }
--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    r = 98 + a ** b
-    return (r)
-    
+    return (98 + a ** b)
